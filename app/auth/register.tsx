4dce87b125bfc279bd038c0d2187298e2f112b27
Ethan <eduvall23@turing.edu>
2026-02-20 18:55:32 -0700

    I forgot why i hated typscirpt

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,26 +1,33 @@
+
+
+
 export default function Register() {
 
-  function signUp (e){
-    e.preventDefault()
-    const formdata = new FormData(e.target)
-    console.log(formdata.get("regiEmail"))
+
+
+
+
+  function signUp(e: Event) {
+    e.preventDefault();
+    const formdata = new FormData(e.currentTarget as HTMLFormElement);
+    const registeredEmail : string = formdata.get("regiEmail") as string;
   }
 
-
-  return(
-  <form onSubmit ={(e)=>signUp(e)}>
-    <label>
-      Email
-      <input name ='regiEmail' placeholder="Email" type="email"></input>
-    </label>
-    <label>
-      Password <input name ='regiPass' placeholder="Password" type="password"></input>
-    </label>
-    <label>
-
-      Please verify your password
-      <input name ='regiPassV' placeholder="Password" type="password"></input>
-    </label>
-    <button type='submit'>Sign up!</button>
-  </form>)
+  return (
+    <form onSubmit={(e) => signUp(e)}>
+      <label>
+        Email
+        <input name="regiEmail" placeholder="Email" type="email"></input>
+      </label>
+      <label>
+        Password{" "}
+        <input name="regiPass" placeholder="Password" type="password"></input>
+      </label>
+      <label>
+        Please verify your password
+        <input name="regiPassV" placeholder="Password" type="password"></input>
+      </label>
+      <button type="submit">Sign up!</button>
+    </form>
+  );
 }
