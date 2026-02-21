df40936ced33573fd4383ebc170469c0e90c3253
Ethan <eduvall23@turing.edu>
2026-02-20 18:38:16 -0700

    Create registartion form

--- app/auth/register.tsx
+++ app/auth/register.tsx
@@ -1,13 +1,18 @@
-
-
-
 export default function Register() {
-  return 
-  (
-    <form>
-      <input></input>
-      <input></input>
-      <input></input>
-    </form>
-  );
+  return;
+  <form>
+    <label>
+      
+      Email
+      <input placeholder="Email" type="email"></input>
+    </label>
+    <label>
+      Password <input placeholder="Password" type="password"></input>
+    </label>
+    <label>
+
+      Please verify your password
+      <input placeholder="Password" type="password"></input>
+    </label>
+  </form>;
 }
