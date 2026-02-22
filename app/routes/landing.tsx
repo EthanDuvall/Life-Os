fd40cae76345977ef7d06775f5afbc1b1392aba3
Ethan <eduvall23@turing.edu>
2026-02-21 23:29:09 -0700

    Style landing page still need to figure out which image i will use if any

--- app/routes/landing.tsx
+++ app/routes/landing.tsx
@@ -18,8 +18,11 @@ export default function Landing() {
           Put your life on track with Life OS goal tracking and progress
           graphing
         </h2>
-        <NavLink className='' to="/register">Sign Up!</NavLink>
-        <NavLink className='' to="/login">Log in</NavLink>
+        <div className='flex flex-row max-w-90 justify-around m-10 items-center'>
+        <NavLink className='border-2 rounded-sm text-3xl p-2' to="/register">Sign Up!</NavLink>
+        <p className='text-xl'>Or</p>
+        <NavLink className='border-2 rounded-sm text-3xl p-2' to="/login">Log in</NavLink>
+        </div>
       </div>
       <img src="" alt="Who even knows" />
       </main>
